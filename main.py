4d3687a368e6102c85afa40da3eb2186876cd4d1
trickypr <trickypr@icloud.com>
2022-05-16 10:22:05 +1000

    ✨ Code to interact with the DHT22
    This code has not been tested on actual hardware as I do not currently
    have access to the raspberry pi that will be used for this project

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
+import Adafruit_DHT
+import time
+
 from api import API
 from credentials import STATION_ID, STATION_KEY
 
+####################################
+# Config variables for the program #
+####################################
 
+# API information
+#
+# For credentials, create a new file called credentials.py
+# and add the constants STATION_ID and STATION_KEY
+UPDATE_FREQ = 2.5 # NOTE: Cannot be any less than 2.5
+
+# Temperature and humidity sensor
+TEM_SENSOR_TYPE = Adafruit_DHT.DHT22
+TEM_PIN = 4
+
+# Requires the adafruit DHT library to be installed. Run the
+# following command to install it:
+# pip3 install --install-option="--force-pi" Adafruit_DHT
+def get_tem_and_humid() -> (float, float):
+    return Adafruit_DHT.read_retry(TEM_SENSOR_TYPE, TEM_PIN)
+
+# TODO: Enable rapid updates, the code for this is currently on a
+# school raspberry pi that I do not have access to.
 api = API(STATION_ID, STATION_KEY)
+
+while True:
+    temperature, humidity = get_tem_and_humid()
+
+    res = api.start_request().temperature_celsius(temperature).humidity(humidity).send()
+    print(res)
+
+    time.sleep(UPDATE_FREQ)
+
+
+
 
 print(api.start_request().temperature_celsius(25.0).send())
