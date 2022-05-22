537a52313509ecd3b4d5a8265bb0252bc16f604b
TrickyPR <trickypr@icloud.com>
2022-05-22 18:32:43 +1000

    ✨ Wind direction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # NOTE: This progrem requires raspi-blinka to be installed. Folow the instructions on adafruits website: https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi
 # NOTE: This will also require adafruit-circuitpython-lps2x
 # NOTE: Requires https://github.com/neliogodoi/MicroPython-SI1145 to function
+# NOTE: Requires `pip3 install adafruit-circuitpython-mcp3xxx`
 
 import Adafruit_DHT
 import time
@@ -11,12 +12,18 @@ import RPi.GPIO as GPIO
 # Circut python imports
 import board
 import busio
+import digitalio
 import adafruit_lps2x
 from api.src.helpers import WindTracker
 import si1145
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
 from api import API, RainTracker, RainEvent
 from credentials import STATION_ID, STATION_KEY
+
+rain_tracker = RainTracker()
+wind_tracker = WindTracker()
 
 ####################################
 # Config variables for the program #
@@ -35,8 +42,31 @@ TEM_PIN = 4
 # Switch sensors
 RAIN_PIN = 17
 WIND_SPEED_PIN = 27
-
 PRESSURE_CALIBRATION_VALUE = 95
+
+# Wind Direction sensor
+WIND_DIR_SELECT_PIN = 0
+WIND_DIR_CHANEL = MCP.P0
+
+K = 1000
+wind_tracker.load_direction_table([
+    [0, 33 * K],
+    [22.5, 6.57 * K],
+    [45, 8.2 * K],
+    [67.5, 891],
+    [90, 1 * K],
+    [112.5, 688],
+    [135, 2.2 * K],
+    [157.5, 1.41*K],
+    [180, 3.9 * K],
+    [202.5, 3.14 * K],
+    [225, 16 * K],
+    [247.5, 14.12 * K],
+    [270, 120 * K],
+    [292.5, 42.12 * K],
+    [315, 64.9 * K],
+    [337.5, 21.88 * K]
+])
 
 ###################################
 # Automagicly generated constants #
@@ -45,6 +75,7 @@ PRESSURE_CALIBRATION_VALUE = 95
 print("Magic constants")
 
 i2c = busio.I2C(board.SCL, board.SDA)
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
 def get_press_sensor() -> adafruit_lps2x.LPS22:
     return adafruit_lps2x.LPS22(i2c)  
@@ -54,8 +85,9 @@ pressure_sensor = get_press_sensor()
 
 uv_sensor = si1145.SI1145()
 
-rain_tracker = RainTracker()
-wind_tracker = WindTracker()
+adc_cs = digitalio.DigitalInOut(board.D5)
+adc = MCP.MCP3008(spi, adc_cs)
+dir_raw = AnalogIn(adc, WIND_DIR_CHANEL)
 
 #########################
 # Other setup functions #
@@ -88,6 +120,11 @@ def get_uv() -> float:
     # The device returns the UV index miltiplied by 100. 
     return uv_sensor.readUV() / 100
 
+def get_wind_direction():
+    voltage = dir_raw.voltage
+    resistance = (voltage * 10_000)/(3.3 - voltage)
+    return wind_tracker.get_direction(resistance)
+
 def rain_callback(*args):
     """
     This function should be called by a hardware interupt whenever the rain sensor is trigured
@@ -98,12 +135,13 @@ def rain_callback(*args):
 
 def wind_callback(*args):
     print("Wind event")
-    wind_tracker.add_event(datetime.now())
+    wind_tracker.add_event(datetime.now(), get_wind_direction())
 
 #############
 # Main Loop #
 #############
 
+print("Loading api")
 api = API(STATION_ID, STATION_KEY).use_realtime(UPDATE_FREQ)
 
 GPIO.add_event_detect(RAIN_PIN, GPIO.FALLING, callback=rain_callback, bouncetime=100)
@@ -113,7 +151,6 @@ while True:
     humidity, temperature = get_tem_and_humid()
     pressure = get_pressure()
     uv = get_uv()
-    rain = rain_tracker.get_past_hour()
 
     res = api.start_request().temperature_celsius(temperature).humidity(
         humidity).pressure_hpa(pressure).uv_index(uv).rain(rain_tracker).wind(wind_tracker).send()
